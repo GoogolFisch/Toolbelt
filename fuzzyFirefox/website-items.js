22e4d3e53058b14a6fbe93e73556e723b9da8c8f
GoogolFisch <94230796+GoogolFisch@users.noreply.github.com>
2026-06-09 20:27:43 +0200

    adding extra example

--- fuzzyFirefox/website-items.js
+++ fuzzyFirefox/website-items.js
@@ -2,4 +2,6 @@ const items = [
   ["github","https://github.com/"],
   // add more here
   ["example","https://example.com"],
+  // with search-term
+  ["youtube#","https://youtube.com/search?q=#"],
 ];
