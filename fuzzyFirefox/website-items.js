7afd319a394f7d754af08fb0293fb09da9f2b495
GoogolFisch <94230796+GoogolFisch@users.noreply.github.com>
2026-07-25 16:52:20 +0200

    Delete fuzzyFirefox/website-items.js

--- fuzzyFirefox/website-items.js
+++
@@ -1,7 +0,0 @@
-const items = [
-  ["github","https://github.com/"],
-  // add more here
-  ["example","https://example.com"],
-  // with search-term
-  ["youtube#","https://youtube.com/search?q=#"],
-];
